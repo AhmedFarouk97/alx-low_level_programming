8417e9c29ba6f7195138e6c6dfb645a22ee32a32
AhmedFarouk97 <Ahmedfarouk1997@yahoo.com>
2023-06-20 13:30:03 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+
+	/**
+	 * print_alphabet - print all alphabet in lowercase
+	 */
+
+
+	void print_alphabet(void)
+	{
+
+	char c[26] = "abcdefghijklmnopqrstuvwxyz";
+
+	for (i = 0, i < 26 , i++)
+	{
+  	putchar(c[i]);
+	}
+	putchar('\n');
+	}

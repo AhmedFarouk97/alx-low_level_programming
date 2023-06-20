714ebbb4ee454af327acffba435682cabe604053
AhmedFarouk97 <Ahmedfarouk1997@yahoo.com>
2023-06-20 14:47:31 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
-
-	/**
-	 * description
-	 * jack_bauer - prints every minute of the day of Jack Bauer
-	 * starting from 00:00 to 23:59
-	 */
+	/* 
+	*
+	* jack_bauer
+	* Description: 'prints every minute of the day of Jack Bauer'
+	* starting from 00:00 to 23:59
+	*
+	* /
 	void jack_bauer(void)
 	{
 		int i, j;

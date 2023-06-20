a196af2b60521f829b00871d6d6ba51bd7fd130c
AhmedFarouk97 <Ahmedfarouk1997@yahoo.com>
2023-06-20 15:05:32 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,22 @@
-#include "main.h"
+#include "holberton.h"
 
-	/**
-	 * main - print _putchar
-	 * Description: 'adding prototype'
-	 * Return: 0
-	 */
-
-
-	int main(void)
-	{
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
+/**
+  * main - Entry point
+  *
+  * Return: Always 0
+  */
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
 	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
+
 	return (0);
-	}
+}

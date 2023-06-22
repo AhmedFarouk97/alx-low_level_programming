34421227438a90fefeee31a6029bc8629ee54bbd
AhmedFarouk97 <Ahmedfarouk1997@yahoo.com>
2023-06-22 12:32:06 -0700

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/*
+ * _isuuper - check of letter is upper or lower 
+ *@c: to be checked
+ *
+ * return  1 when c is uppercase and return 0 when c is lowercase
+ */
+
+
+int _isupper(int c);
+{
+	if (c >= 65 && c <= 90)
+	{
+	return (1);
+	}
+
+	return (0);
+}
